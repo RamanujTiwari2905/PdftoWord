188fd6fac42066b9d6cff12ebf2b63a864ebb235
Ramanuj Tiwari <tiwariramanujk@gmail.com>
2026-05-10 11:25:36 +0530

    Middleware created for file upload and route is also created

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express();
 require('dotenv').config();
+const convertRoutes = require("./routes/convertRoutes");
 
 
 app.use(express.json());
+app.use("/api/convert", convertRoutes);
 
 
 const InitalizeConnection = async ()=>{
---
+++ Backend/src/routes/convertRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/uploadMiddleware")
+
+router.post("/",upload.single("file"), (req, res)=>{
+    res.json({
+        success: true,
+        message: "File uploaded successfully",
+        file : req.file,
+    });
+});
+
+module.exports = router;
